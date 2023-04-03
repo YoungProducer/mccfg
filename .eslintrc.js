3f8ad07510db89936af0aa20568df9d6182bebe4
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-04-03 20:14:54 +0300

    Minor configuration changes.

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,7 @@ module.exports = {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+  ignorePatterns: ['.eslintrc.js', './src/client/**/*.ts?x'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
