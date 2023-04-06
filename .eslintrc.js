820316bcc2bc21160fa2abd8f3108a65ab5de093
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-04-06 23:18:38 +0300

    Updated rule for unused-vars in eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -22,5 +22,14 @@ module.exports = {
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "warn", // or "error"
+      { 
+        "argsIgnorePattern": "^_",
+        "varsIgnorePattern": "^_",
+        "caughtErrorsIgnorePattern": "^_"
+      }
+    ],
   },
 };
