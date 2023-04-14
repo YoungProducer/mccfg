c9df607f4bbdf85f88b1f974b18931bb910f88f7
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-04-14 15:11:37 +0300

    Added imports for refresh module.

--- src/server/domain/tokens/refresh/refresh.module.ts
+++ src/server/domain/tokens/refresh/refresh.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { RefreshService } from './refresh.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'server/domain/users/entities/user.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([RefreshModule, UserEntity])],
   providers: [RefreshService],
   exports: [RefreshService],
 })
