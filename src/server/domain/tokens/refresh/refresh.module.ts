bca33464fb966c7f2440f47363ee7bff9b6ad98a
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-04-14 16:49:51 +0300

    Fixed RefreshModule imports.

--- src/server/domain/tokens/refresh/refresh.module.ts
+++ src/server/domain/tokens/refresh/refresh.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { RefreshService } from './refresh.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from 'server/domain/users/users.module';
+import { RefreshTokenEntity } from '../entities/refresh-token.entity';
 
 @Module({
-  imports: [UsersModule, TypeOrmModule.forFeature([RefreshModule])],
+  imports: [UsersModule, TypeOrmModule.forFeature([RefreshTokenEntity])],
   providers: [RefreshService],
   exports: [RefreshService],
 })
