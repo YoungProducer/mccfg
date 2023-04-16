d1d0a60fd32bbbb80273baddca2eed1dc6fbbe97
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-04-16 21:44:52 +0300

    Removed console.log

--- src/server/domain/tokens/jwt/jwt.service.ts
+++ src/server/domain/tokens/jwt/jwt.service.ts
@@ -24,7 +24,6 @@ export class JWTService {
     try {
       return await verify(token, secret);
     } catch (e) {
-      console.log({ error: e });
       if (e instanceof JsonWebTokenError) {
         throw new UnauthorizedException('Token is invalid!');
       }
