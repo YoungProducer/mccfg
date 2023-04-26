0122d0dc98f7833410ad7f08affa1b4612fd14f8
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-04-26 12:40:34 +0300

    Added `RemoveFileOnFailure` interceptor.

---
+++ src/server/lib/interceptors/remove-file-on-failure/remove-file-on-failure.interceptor.ts
@@ -0,0 +1,41 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Request } from 'express';
+import { existsSync, unlinkSync } from 'node:fs';
+import { join } from 'node:path';
+import { Observable, throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+
+/**
+ * Can be used in pair with `FileInterceptor`
+ *
+ * Will remove created file if request failed for any reasons
+ */
+@Injectable()
+export class RemoveFileOnFailureInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const req = context.switchToHttp().getRequest<Request>();
+
+    return next.handle().pipe(
+      catchError((err) => {
+        const file: Express.Multer.File = req['file'];
+
+        const pathToFile = !!file
+          ? join(process.cwd(), file.destination, file.filename)
+          : undefined;
+
+        const isExist = pathToFile && existsSync(pathToFile);
+
+        if (isExist) {
+          unlinkSync(pathToFile);
+        }
+
+        return throwError(() => err);
+      }),
+    );
+  }
+}
