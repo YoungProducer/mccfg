dffaa36dbc9dcbadf0c98983387b893bd96cb085
Oleksandr Bezrukov <sashabezrukovownmail@gmail.com>
2023-04-27 21:02:11 +0300

    Refactored `ModVersionDto`.

--- src/server/domain/mods/dto/mod-version.dto.ts
+++ src/server/domain/mods/dto/mod-version.dto.ts
@@ -3,26 +3,6 @@ import { Exclude, Expose, Type } from 'class-transformer';
 import { VersionResponseDto } from 'server/domain/mcversion/dto/version.response.dto';
 
 @Exclude()
-export class ModVersionPopulatedDto {
-  @Expose()
-  @ApiProperty()
-  id: number;
-
-  @Expose()
-  @ApiProperty()
-  version: string;
-
-  @Expose()
-  @Type(() => VersionResponseDto)
-  @ApiProperty({
-    type: VersionResponseDto,
-    isArray: true,
-    required: false,
-  })
-  compatibleMCVersions: VersionResponseDto[];
-}
-
-@Exclude()
 export class ModVersionDto {
   @Expose()
   @ApiProperty()
@@ -31,4 +11,16 @@ export class ModVersionDto {
   @Expose()
   @ApiProperty()
   version: string;
+}
+
+@Exclude()
+export class ModVersionPopulatedDto extends ModVersionDto {
+  @Expose()
+  @Type(() => VersionResponseDto)
+  @ApiProperty({
+    type: VersionResponseDto,
+    isArray: true,
+    required: false,
+  })
+  compatibleMCVersions: VersionResponseDto[];
 }
